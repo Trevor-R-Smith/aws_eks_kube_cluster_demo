39f37f3b4e131eda302d8e90e23fc152b5fc74cb
trevor smith <tyrhys1983@hotmail.com>
2019-09-30 11:53:32 +0100

    update the gitignore

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
     backend "s3" {
-    region         = "eu-west-1"
-    bucket         = "gft-payments-app-backend"
-    key            = "terraform.tfstate"
-    dynamodb_table = "gft-payments-app-backend"
+    region         = ""
+    bucket         = ""
+    key            = ""
+    dynamodb_table = ""
     encrypt        = "true"
   }
 }
